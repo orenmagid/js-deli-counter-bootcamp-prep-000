ebade1224740690939d0d24fb2c82042f8d111fd
Oren Magid <autoimpedicis@gmail.com>
2018-03-13 11:50:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  return `Welcome, ${name}. You are number ${name.index} in line.`
+  return `Welcome, ${name}. You are number ${katzDeliLine.index} in line.`
 }

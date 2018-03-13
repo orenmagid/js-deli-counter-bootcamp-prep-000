98ca0530ea2d8512e6058bd39160bf95568ca2fc
Oren Magid <autoimpedicis@gmail.com>
2018-03-13 11:52:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  return `Welcome, ${name}. You are number ${katzDeliLine.index + 1} in line.`
+  return `Welcome, ${name}. You are number [katzDeliLine.index + 1] in line.`
 }

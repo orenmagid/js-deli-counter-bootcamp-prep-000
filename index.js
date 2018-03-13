03e18d91e6802030e93123aeb3c60b9e820acc05
Oren Magid <autoimpedicis@gmail.com>
2018-03-13 12:00:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+
+function nowServing() {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return katzDeliLine.shift()
+  }
+}
